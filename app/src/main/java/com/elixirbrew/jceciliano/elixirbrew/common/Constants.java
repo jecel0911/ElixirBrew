40788a5dd61f7ca85435b6c7043379951705058c
Jeremias Ceciliano <jecel0911@gmail.com>
2016-12-21 20:52:03 -0600

    add Constants class

---
+++ app/src/main/java/com/elixirbrew/jceciliano/elixirbrew/common/Constants.java
@@ -0,0 +1,13 @@
+package com.elixirbrew.jceciliano.elixirbrew.common;
+
+/**
+ * Created by jceciliano on 21/12/16.
+ */
+
+public class Constants {
+    public static final String BREWERY_API = "http://api.brewerydb.com";
+    public static final String BEER_SEARCH = "/v2/beers/";
+    public static final String QUERY_SEARCH = "name";
+
+    private Constants() {}
+}
